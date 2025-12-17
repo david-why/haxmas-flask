1aa1605c251644d8de7693975d55af29f37f8d74
david-why <david_why@outlook.com>
2025-12-17 21:04:27 +0800

    initial commit

---
+++ static/main.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("giftForm");
+const giftsContainer = document.getElementById("gifts");
+
+async function loadGifts() {
+    const response = await fetch('/gifts');
+    const gifts = await response.json();
+    
+    giftsContainer.innerHTML = '';
+    gifts.forEach(gift => {
+        const item = document.createElement("p");
+        item.classList.add('text-primary')
+        item.classList.add('fs-5')
+        item.textContent = `Gift for ${gift.name}: ${gift.gift}! 🎉`;
+        giftsContainer.appendChild(item);
+    });
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const name = form.elements.name.value;
+    const gift = form.elements.gift.value;
+
+    await fetch('/gifts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, gift })
+    });
+
+    form.reset();
+    await loadGifts(); 
+});
+
+loadGifts();
